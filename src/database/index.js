46d7f1b4d52d3569e20ea8ba78458b6253405646
Alisson Michel Sganzerla <alisson_sganzerla@yahoo.com.br>
2020-10-04 01:09:48 -0300

    fix(database): add prop remove warning obsolet method findAndUpdateById

--- src/database/index.js
+++ src/database/index.js
@@ -2,9 +2,10 @@ import pkg from 'mongoose';
 const { set, connect } = pkg;
 
 set('useCreateIndex', true);
-connect('mongodb://localhost:27017/apisendemail',{
-    useNewUrlParser:true,
-    useUnifiedTopology: true
+connect('mongodb://localhost:27017/apisendemail', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
 });
 
 export default pkg;
