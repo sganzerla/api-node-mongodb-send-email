10b5a8bb3f0f65d64e208a96a679b232fc8e6e1b
Alisson Michel Sganzerla <alisson_sganzerla@yahoo.com.br>
2020-10-05 00:10:44 -0300

    chore(router): add route reset_password

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import pkg from 'express';
 const { Router } = pkg;
-import { forgout, login, store } from './app/controller/authController.js';
+import { forgout, login, reset, store } from './app/controller/authController.js';
 import { index } from './app/controller/projectController.js';
 import  authMidleware  from './app/midleware/auth.js';
 
@@ -10,7 +10,7 @@ const router = Router();
 router.post('/auth/register', store);
 router.post('/auth/authenticate', login);
 router.post('/auth/forgout_password', forgout);
-
+router.post('/auth/reset_password', reset);
 router.use(authMidleware).get('/project', index);
 
 export default router;
