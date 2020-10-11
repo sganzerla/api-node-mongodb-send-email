da446dc8bfcd7802a5e3d0cc2e9d79b7a5972d2f
Alisson Michel Sganzerla <alisson_sganzerla@yahoo.com.br>
2020-10-11 19:43:59 -0300

    feat(project): add controller project

--- src/app/controller/projectController.js
+++ src/app/controller/projectController.js
@@ -1,4 +1,59 @@
+import Project from '../models/project.js';
+import Task from '../models/task.js';
 
 export async function index(req, res) {
-    return res.send({ message: 'ok', user: req.userId })
+
+    try {
+        const projects = await Project.find().populate(['user', 'tasks']);
+
+        return res.send({ projects });
+    } catch (error) {
+        return res.status(400).send({ error: 'Error loading projects' });
+    }
+}
+
+export async function show(req, res) {
+    try {
+        const project = await Project.findById(req.params.projectId).populate(['user', 'tasks']);
+
+        return res.send({ project });
+    } catch (error) {
+        return res.status(400).send({ error: 'Error loading project' });
+    }
+}
+
+export async function update(req, res) {
+    return res.send();
+}
+
+export async function store(req, res) {
+    try {
+
+        const { title, description, tasks } = req.body;
+
+        const project = await Project.create({ title, description, user: req.userId });
+
+        await Promise.all(tasks.map(async task => {
+            const projectTask = new Task({ ...task, project: project._id });
+            await projectTask.save();
+            project.tasks.push(projectTask);
+        }));
+
+        await project.save();
+
+        return res.send({ project });
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send({ error: 'Error creating new project' });
+    }
+}
+
+export async function remove(req, res) {
+    try {
+        await Project.findByIdAndRemove(req.params.projectId).populate('user');
+
+        return res.send();
+    } catch (error) {
+        return res.status(400).send({ error: 'Error loading project' });
+    }
 }
--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import pkg from 'express';
 const { Router } = pkg;
 import { forgout, login, reset, store } from './app/controller/authController.js';
-import { index } from './app/controller/projectController.js';
-import  authMidleware  from './app/midleware/auth.js';
+import { show, index, update, store as pStore, remove } from './app/controller/projectController.js';
+import authMidleware from './app/midleware/auth.js';
 
 
 const router = Router();
@@ -11,6 +11,11 @@ router.post('/auth/register', store);
 router.post('/auth/authenticate', login);
 router.post('/auth/forgout_password', forgout);
 router.post('/auth/reset_password', reset);
-router.use(authMidleware).get('/project', index);
 
+
+router.use(authMidleware).get('/project', index);
+router.use(authMidleware).get('/project/:projectId', show);
+router.use(authMidleware).post('/project', pStore);
+router.use(authMidleware).put('/project/:projectId', update);
+router.use(authMidleware).delete('/project/:projectId', remove);
 export default router;
